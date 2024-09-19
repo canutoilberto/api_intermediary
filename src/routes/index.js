e8b1411876d3676b4684cb752de38c99c548e417
canutoilberto <canutoilberto@gmail.com>
2024-09-19 11:09:07 -0300

    [Feat] All routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,16 @@
 const { Router } = require("express");
 const gridRouter = require("./gridRouter");
+const programRouter = require("./programRouter");
+const durationsRouter = require("./durationsRouter");
+const abrangenciesRouter = require("./abrangenciesRouter");
+const priceRouter = require("./priceRouter");
 
 const router = Router();
 
 router.use("/pricing/external/grid", gridRouter);
+router.use("/pricing/external/base/programs", programRouter);
+router.use("/pricing/external/base/durations", durationsRouter);
+router.use("/pricing/external/abrangencies", abrangenciesRouter);
+router.use("/pricing/external/price", priceRouter);
 
 module.exports = router;
